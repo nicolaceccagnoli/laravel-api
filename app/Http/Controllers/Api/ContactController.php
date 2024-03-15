90d36a6ec289b1dbecd944ec4bbf5171813e5733
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-03-15 15:18:45 +0100

    Add Contact Model, Migration, Request, Controller

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models 
+use App\Models\Contact;
+
+// Form Request
+use App\Http\Requests\Contact\StoreRequest as ContactStoreRequest;
+
+
+class ContactController extends Controller
+{
+    public function store(ContactStoreRequest $request) {
+        $contact = Contact::create($request->validated);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Contatto salvato'
+        ]);
+    }
+}
---
+++ app/Http/Requests/Contact/StoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Contact;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:64',
+            'email' => 'required|email|max:255',
+            'message' => 'required|max:2048',
+            'accepted' => 'required|boolean|accepted'
+        ];
+    }
+}
