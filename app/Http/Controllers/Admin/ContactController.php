96307eb7c957e866b2df4cd56ce6786f8a785130
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-03-15 18:28:30 +0100

    Add Form in contacts.index

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -12,7 +12,26 @@ class ContactController extends Controller
 {
     public function index() {
 
-        $contacts = Contact::all();
+        // $contacts = Contact::all();
+
+        // Creo una query che indipendentemente dall'inserimento di dati negli input
+        // mi restituirà di base tutti i contatti
+        $contactsQuery = Contact::select('*');
+
+        $queryStringParams = request()->all();
+
+        // Se sto filtrando per nome
+        if(isset($queryStringParams['name'])) {
+            $contactsQuery->where('name', 'LIKE', '%'.$queryStringParams['name'].'%');
+        }
+
+        // Se sto filtrando per mail
+        if(isset($queryStringParams['email'])) {
+            $contactsQuery->where('email', 'LIKE', '%'.$queryStringParams['email'].'%');
+        }
+        
+        // Eseguo la query
+        $contacts = $contactsQuery->get();
 
         return view('admin.contacts.index', compact('contacts'));
     }
--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -5,6 +5,19 @@
 @section('main-content')
     <section id="contacts-index">
         <div class="row g-0">
+
+            <form action="{{ route('admin.contacts.index') }}" method="GET">
+                <div class="input-group my-3">
+                    <span class="input-group-text">Cerca per Nome</span>
+                    <input type="text" aria-label="First name" class="form-control" id="name" name="name" value="{{ request()->input('name') }}">
+                    <span class="input-group-text">Cerca per Mail</span>
+                    <input type="text" aria-label="Last name" class="form-control" id="email" name="email" value="{{ request()->input('email') }}">
+                    <button type="submit" class="btn btn-primary">
+                        Cerca
+                    </button>    
+                </div>
+            </form>
+
             @foreach ($contacts as $contact)
                 <div class="d-flex justify-content-center col-12 col-xs-6 col-sm-4 col-md-3 mb-3">
                     <div class="my-card m-1">
